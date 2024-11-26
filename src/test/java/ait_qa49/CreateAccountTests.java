ba18763601f7b84c29bfcdae44ddb1c2ffd114f2
Kateryna-Komarova <svonsonket@gmail.com>
2024-11-26 13:55:01 +0100

    Homework_08

--- src/test/java/ait_qa49/CreateAccountTests.java
+++ src/test/java/ait_qa49/CreateAccountTests.java
@@ -2,6 +2,7 @@ package ait_qa49;
 
 import demo.webshop.data.NewUserData;
 import demo.webshop.models.Contact;
+import demo.webshop.utils.DataProviders;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -26,6 +27,16 @@ public class CreateAccountTests extends TestBase {
                 .setEmail(NewUserData.Email)
                 .setPassword(NewUserData.Password)
                 .setConfirmPassword(NewUserData.ConfirmPassword));
+        app.getContact().clickRegistrationEnd();
+        Assert.assertTrue(app.getContact().isElementPresent(By.cssSelector("input[value='Continue']")));
+
+    }
+
+    @Test(dataProvider = "createAccountWithCsv", dataProviderClass = DataProviders.class)
+    public void addContactWithCsvFileTest(Contact contact) {
+        int i = (int) ((System.currentTimeMillis() / 1000) % 3600);
+        app.getContact().clickOnRegisterButton();
+        app.getContact().fillRegistrationData(contact);
         app.getContact().clickRegistrationEnd();
         Assert.assertTrue(app.getContact().isElementPresent(By.cssSelector("input[value='Continue']")));
 
