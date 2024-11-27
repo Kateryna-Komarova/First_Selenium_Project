7fe4a24e21b34bfbd6a7909e6d39d181f5ca8a20
Kateryna-Komarova <svonsonket@gmail.com>
2024-11-27 16:22:44 +0100

    build

--- src/main/java/demo/webshop/data/NewUserData.java
+++ src/main/java/demo/webshop/data/NewUserData.java
@@ -1,9 +1,9 @@
 package demo.webshop.data;
 
 public class NewUserData {
-    public static  final  String FirstName = "Kwzrkohfs";
-    public static  final  String LastName = "Mpozlipr";
-    public static  final  String Email = "Kwelbto@gmail.com";
+    public static  final  String FirstName = "Kwzruzohfs";
+    public static  final  String LastName = "Mpopitr";
+    public static  final  String Email = "Kwopzto@gmail.com";
     public static  final  String Password = "@NpnD!A8gc99uo";
     public static  final  String ConfirmPassword = "@NpnD!A8gc99uo";
 }
